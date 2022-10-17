ed7f95b213123ac2cb0c1a7453ba30782a95a215
varsh <varsh@LAPTOP-12SM487F>
2022-10-17 12:21:30 +0530

    first commit

---
+++ JavaProjects/src/RelationalOperators.java
@@ -0,0 +1,19 @@
+import java.util.Random;
+public class RelationalOperators {
+
+	public static void main(String[] args) {
+	
+				Random rand =new Random();
+				int i=rand.nextInt(100);
+				int j=rand.nextInt(100);
+				System.out.println("i ="+i);
+				System.out.println("j ="+j);
+				System.out.println(i>j);
+				System.out.println(i<j);
+			}
+
+		
+
+	}
+
+
