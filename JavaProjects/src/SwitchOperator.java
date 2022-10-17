d713a52e12ff7f549c45852c98594ecba06e864f
varsh <varsh@LAPTOP-12SM487F>
2022-10-17 15:34:52 +0530

    first commit

---
+++ JavaProjects/src/SwitchOperator.java
@@ -0,0 +1,18 @@
+
+public class SwitchOperator {
+
+	public static void main(String[] args) {
+		int i=0;
+		switch(i) {
+		case 0:
+			System.out.println("i is 0");
+		case 1:
+			System.out.println("i is 1");
+		case 2:
+			System.out.println("i is 2");
+		}
+
+
+	}
+
+}
