b3459f016f3902834aa6cb16479537e623b03753
varsh <varsh@LAPTOP-12SM487F>
2022-10-17 15:27:42 +0530

    first commit

---
+++ JavaProjects/src/IncrementOperator.java
@@ -0,0 +1,15 @@
+
+public class IncrementOperator {
+
+	public static void main(String[] args) {
+		int numA=5;
+		int numB=10;
+		int numC=0;
+
+		    System.out.println(numA);
+		    System.out.println(numB);
+		     System.out.println(numC);
+
+	}
+
+}
