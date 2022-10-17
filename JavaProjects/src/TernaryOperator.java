63fcde3e01a003159730281c2eb98738f6086b8f
varsh <varsh@LAPTOP-12SM487F>
2022-10-17 15:43:14 +0530

    first commit

---
+++ JavaProjects/src/TernaryOperator.java
@@ -0,0 +1,12 @@
+
+public class TernaryOperator {
+
+	public static void main(String[] args) {
+		int v=1;
+		System.out.println(v==1 ? "Hello":"World");
+		v++;
+		System.out.println(v==1 ? "Hello":"World");
+
+	}
+
+}
