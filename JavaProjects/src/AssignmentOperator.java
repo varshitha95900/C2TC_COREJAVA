39f57442d16ca1a9fd441cc4669476da8cbd3bb7
varsh <varsh@LAPTOP-12SM487F>
2022-10-17 15:19:01 +0530

    first commit

---
+++ JavaProjects/src/AssignmentOperator.java
@@ -0,0 +1,14 @@
+
+public class AssignmentOperator {
+
+	public static void main(String[] args) {
+		int a=1;
+		int b=2;
+		int c=3;
+		System.out.println(a);
+		System.out.println(b);
+		System.out.println(c);
+		}
+	}
+
+
