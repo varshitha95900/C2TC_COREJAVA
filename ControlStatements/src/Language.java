b8157fd56a69a377b689541e9d167d59c77ed144
varsh <varsh@LAPTOP-12SM487F>
2022-10-22 09:13:10 +0530

    first commit

---
+++ ControlStatements/src/Language.java
@@ -0,0 +1,19 @@
+
+public class Language {
+
+	public static void main(String[] args) {
+		String lang ="java";
+		if(lang =="python") {
+			System.out.println("languange is python");
+		}
+		else if (lang =="c++") {
+			System.out.println("lang is c++");
+		}else if(lang =="java") {
+			System.out.println("lang is java");
+		}else {
+			System.out.println(lang);
+			
+		}
+	}
+
+}
---
+++ ControlStatements/src/Main1.java
@@ -0,0 +1,10 @@
+
+public class Main1 {
+
+	public static void main(String[] args) {
+		for(;;) {
+			System.out.println("hi");
+		}
+	}
+
+}
