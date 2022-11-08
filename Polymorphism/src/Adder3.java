cbd7fa604062cf06bf2bd98ce5598d176cb61e04
varsh <varsh@LAPTOP-12SM487F>
2022-11-08 17:03:06 +0530

    first commit

---
+++ Polymorphism/src/Adder3.java
@@ -0,0 +1,19 @@
+
+public class Adder3 {
+	static int add1(int a,int b)
+	{
+		return a+b;
+	}
+	static double add(int a,int b)
+	{
+		return a+b;
+	}
+}
+class testoverloading3
+{
+	
+	public static void main(String[] args) {
+	System.out.println(Adder3.add1(11,11));	
+	}
+
+}
