dd419ecb003f29aef44e50838b614703df66460c
varsh <varsh@LAPTOP-12SM487F>
2022-11-08 12:31:20 +0530

    first commit

--- StaticKeyword/src/Demo.java
+++ StaticKeyword/src/Demo.java
@@ -1,9 +1,11 @@
 
 public class Demo {
-
+	static
+	{
+		System.out.println("static block is invoked");
+	}
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		System.out.println("hello main");
 	}
 
 }
