f15fa109a30dcf78599990e3e150d8fdd394c174
varsh <varsh@LAPTOP-12SM487F>
2022-11-08 16:39:09 +0530

    first commit

---
+++ Inheritance/src/canimal.java
@@ -0,0 +1,23 @@
+
+public class canimal {
+canimal()
+{
+	System.out.println("animal is created");
+}
+}
+class Dog extends canimal
+{
+	Dog()
+	{
+		super();
+		System.out.println("dog is created");
+	}
+}
+class Demo
+{
+	public static void main(String args[]) {
+		dog d=new dog();
+		
+	}
+
+}
