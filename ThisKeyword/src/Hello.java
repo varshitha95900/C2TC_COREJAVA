9394eeebf16d1f3c09eb9c8e2afd95c4ce8bb8b9
varsh <varsh@LAPTOP-12SM487F>
2022-11-08 13:00:15 +0530

    first commit

---
+++ ThisKeyword/src/Hello.java
@@ -0,0 +1,21 @@
+
+public class Hello {
+	Hello()
+	{
+		this(5);
+		System.out.println("Hello a");
+	}
+	Hello(int x)
+	{
+		System.out.println(x);
+	}
+}
+class main
+{
+	public static void main(String args[]) {
+		Hello a=new Hello();
+		
+
+	}
+
+}
