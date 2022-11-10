cbbccb1fb58297746292e511c21cfce193f3346e
varsh <varsh@LAPTOP-12SM487F>
2022-11-10 22:20:44 +0530

    first commit

---
+++ AbstractClass/src/shape.java
@@ -0,0 +1,20 @@
+
+public abstract class shape {
+abstract void draw();
+}
+class rectangle extends shape {
+	void draw() {
+		System.out.println("drawing rectangle");
+	}
+	}
+class circle extends shape {
+	void draw() {
+		System.out.println("drawing circle");
+	}
+	
+	public static void main(String args[]) {
+		shape s=new rectangle();
+		s.draw();
+	}
+
+}
