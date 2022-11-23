b2ba7256ace61e981b989d24488e4ed9ffebe6e6
varsh <varsh@LAPTOP-12SM487F>
2022-11-23 10:47:01 +0530

    first commit

---
+++ Collection/src/Main1.java
@@ -0,0 +1,26 @@
+
+public class Main1 {
+
+	public static void main(String[] args) {
+		Object list;
+		list <Integer> 1=new arraylist<integer>();
+		1.add(1);
+		1.add(2);
+		1.add(3);
+		1.add(4);
+		1.add(5);
+		1.add(5);
+		1.add(4);
+		System.out.println(1);
+
+	}
+
+	
+	}
+
+	
+		
+		
+	
+
+
