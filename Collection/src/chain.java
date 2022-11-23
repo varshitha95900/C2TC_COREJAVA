239620a5d89f77d4e8955d6a3ecfde4c1034b8ff
varsh <varsh@LAPTOP-12SM487F>
2022-11-23 11:15:05 +0530

    first commit

---
+++ Collection/src/chain.java
@@ -0,0 +1,14 @@
+
+
+public class chain {
+
+	public static void main(String[] args) {
+		List<Integer> list=new List<Integer>();
+		list.add(1);
+		list.add(4);
+		list.add(7);
+		list.add(5);
+		System.out.println(list);
+	}
+
+}
