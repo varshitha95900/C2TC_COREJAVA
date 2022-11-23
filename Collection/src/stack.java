6778ee547d05086c0c397b6b60a4ab54488c78a0
varsh <varsh@LAPTOP-12SM487F>
2022-11-23 10:40:52 +0530

    first commit

---
+++ Collection/src/stack.java
@@ -0,0 +1,14 @@
+import java.util.Collection;
+
+public class stack {
+
+	public static void main(String[] args) {
+		Collection<Integer> i=(Collection<Integer>) new arraylist();
+		i.add(10);
+		i.add(20);
+		i.add(55);
+		i.add(2);
+		System.out.println(i);
+	}
+
+}
