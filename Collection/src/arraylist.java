62fd1567dcdf08b61cf9e66469d361beb62b6d78
varsh <varsh@LAPTOP-12SM487F>
2022-11-23 10:36:25 +0530

    first commit

--- Collection/src/arraylist.java
+++ Collection/src/arraylist.java
@@ -1,9 +1,14 @@
+import java.util.Collection;
 
 public class arraylist {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Collection list=(Collection) new arraylist();
+				list.add(100);
+		list.add(20);
+		list.add(2000);
+		list.add("webinar");
+		System.out.println("arraylist :"+list);
 	}
 
 }
