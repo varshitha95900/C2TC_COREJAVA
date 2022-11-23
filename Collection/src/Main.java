c770d8946bd4c7b4bca5cac2034ee7c1f86c9d4c
varsh <varsh@LAPTOP-12SM487F>
2022-11-23 10:30:03 +0530

    first commit

--- Collection/src/Main.java
+++ Collection/src/Main.java
@@ -2,8 +2,15 @@
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		int arr[]=new int[8];
+		arr[0]=10;
+		arr[1]=20;
+		arr[2]=60;
+		arr[3]=80;
+		arr[4]=100;
+		arr[5]=(int) 9.2f;
+		for(int i=0;i<arr.length;i++)
+			System.out.println(arr[i]);
 	}
 
 }
