9f05393602b91c6b2851607f50968ae44c42b345
varsh <varsh@LAPTOP-12SM487F>
2022-11-23 10:54:49 +0530

    first commit

---
+++ Collection/src/method.java
@@ -0,0 +1,20 @@
+
+
+public class method {
+
+	public static void main(String[] args) {
+	list<integer> val=new arraylist();
+		val.add(0);
+		val.add(11);
+		val.add(23);
+		val.add(34);
+		val.add(45);
+		for(Integer e : val)
+			{
+			System.out.print(e);
+			}
+		System.out.println("arraylist :"+val);
+			
+			}
+
+}
